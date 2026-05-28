f05631e368d10bf1a8af4122dcd630402bc3f00a
jack_os <hello@designwithjack.com>
2026-05-28 14:00:33 +0100

    fix typo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -27,7 +27,7 @@
           <ExperienceItem
             company="Speckle"
             role="Product Designer"
-            dates="May 2025 — April 2026"
+            dates="May 2025 — Apr 2026"
           >
             <p>
               Speckle is a Series A startup building software used by thousands
