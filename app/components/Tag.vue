045329d62bfd2cfe347baa7202fb1731366a651c
jack_os <hello@designwithjack.com>
2026-06-09 13:05:44 +0100

    header and footer updates

---
+++ app/components/Tag.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="text-base bg-neutral-800 rounded-full pl-[11px] pr-3 leading-none h-6 flex items-center w-fit"
+  >
+    {{ label }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+}>();
+</script>
